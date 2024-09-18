1ca8e7ecbc18a5d927f2413570bfb6cd9dc420f4
keiv <vudangkhoi1994@gmail.com>
2024-09-18 16:57:00 +0700

    add eslint

---
+++ part3/anecdotes-backend/eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from 'globals';
+import stylisticJs from '@stylistic/eslint-plugin-js';
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: 'latest',
+    },
+    plugins: {
+      '@stylistic/js': stylisticJs,
+    },
+    rules: {
+      eqeqeq: 'error',
+    },
+  },
+  { languageOptions: { globals: globals.node } },
+  {
+    ignores: ['dist/**', 'build/**'],
+  },
+];
