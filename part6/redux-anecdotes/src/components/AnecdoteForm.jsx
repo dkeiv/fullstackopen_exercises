44981a623090c30ba30e548f5ec1f4f8e802b122
vudangkhoi1994 <vudangkhoi1994@gmail.com>
2024-10-07 22:54:33 +0700

    part6a ex6.3-6.8

---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,27 @@
+import { useDispatch } from 'react-redux';
+import { creatNote } from '../reducers/anecdoteReducer';
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch();
+
+  const createNewNote = e => {
+    e.preventDefault();
+    const anecdote = e.target.note.value;
+    e.target.note.value = '';
+    dispatch(creatNote(anecdote));
+  };
+
+  return (
+    <>
+      <h2>create new</h2>
+      <form onSubmit={createNewNote}>
+        <div>
+          <input type='text' name='note' />
+        </div>
+        <button type='submit'>create</button>
+      </form>
+    </>
+  );
+};
+
+export default AnecdoteForm;
